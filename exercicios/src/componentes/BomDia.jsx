2a451246e0a749f1b1630c8e172827a9c59ea06e
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-11-12 17:17:37 -0300

    init

---
+++ exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/* export default props => 
+    <React.Fragment>
+        <h1>Bom Dia {props.nome}!!! </h1>
+        <h1>Até mais</h1>
+    </React.Fragment> */
+
+export default props => [
+    <h1 key='h1'>Bom Dia {props.nome}!!! </h1>,
+    <h1 key='h2'>Até mais</h1>  
+]  
+    
---
+++ exercicios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/* import Blabla from './componentes/Primeiro' */
+import BomDia from './componentes/BomDia'
+
+/* ReactDOM.render(<Blabla />, document.getElementById(`root`)) */
+ReactDOM.render(<BomDia  nome="Julio"/>, document.getElementById(`root`))
