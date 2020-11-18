2a68061ebfad54d4150529725ac20ea0a9eb6170
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-11-18 14:57:38 -0300

    class-in-react

--- exercicios/src/componentes/BomDia.jsx
+++ exercicios/src/componentes/BomDia.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 /* export default props => 
+    <div>
+        <h1>Bom Dia {props.nome}!!! </h1>
+        <h1>Até mais</h1>
+    </div> */
+
+/* export default props => 
     <React.Fragment>
         <h1>Bom Dia {props.nome}!!! </h1>
         <h1>Até mais</h1>
--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -2,7 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 /* import Blabla from './componentes/Primeiro' */
-import BomDia from './componentes/BomDia'
+/* import BomDia from './componentes/BomDia' */
 
 /* ReactDOM.render(<Blabla />, document.getElementById(`root`)) */
-ReactDOM.render(<BomDia  nome="Julio"/>, document.getElementById(`root`))
+/* ReactDOM.render(<BomDia  nome="Julio"/>, document.getElementById(`root`)) */
+
+/* import Multi from './componentes/Multiplos'
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Julio"/>
+        <Multi.BoaNoite nome="Julia"/>
+    </div>
+, document.getElementById('root')) */
+
+/* import Saudacao from './componentes/Saudacao'
+
+ReactDOM.render(
+    <div>
+        <Saudacao tipo="Bom Dia" nome="Caio" />
+    </div>
+    , document.getElementById('root')
+) */
+
+import Pai from './componentes/pai'
+import Filho from './componentes/filho'
+
+ReactDOM.render(
+    <div>
+        <Pai nome="Carlos" sobrenome="Johnson">
+            {/* Como passo os componentes filho aqui? */}
+            <Filho nome="Carlito" />
+            <Filho nome="Rick" />
+            <Filho nome="Lila" />
+        </Pai>
+    </div>
+,document.getElementById('root'))
